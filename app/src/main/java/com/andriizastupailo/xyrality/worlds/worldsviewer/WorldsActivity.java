fcf895ed4326c506b926274cca69bc8f1b7a6e3f
ayvengoza <ayvengoza@gmail.com>
2018-02-06 20:57:34 +0200

    create application

---
+++ app/src/main/java/com/andriizastupailo/xyrality/worlds/worldsviewer/WorldsActivity.java
@@ -0,0 +1,13 @@
+package com.andriizastupailo.xyrality.worlds.worldsviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorldsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worlds);
+    }
+}
