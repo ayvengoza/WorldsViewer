ba9237bdbdf41a821945025a07943c2729569b5b
ayvengoza <ayvengoza@gmail.com>
2018-02-06 22:02:25 +0200

    add recyclerview for worlds display

---
+++ app/src/main/java/com/andriizastupailo/xyrality/worlds/worldsviewer/World.java
@@ -0,0 +1,8 @@
+package com.andriizastupailo.xyrality.worlds.worldsviewer;
+
+/**
+ * Created by andriizastupailo on 06.02.18.
+ */
+
+public class World {
+}
--- app/src/main/java/com/andriizastupailo/xyrality/worlds/worldsviewer/WorldsFragment.java
+++ app/src/main/java/com/andriizastupailo/xyrality/worlds/worldsviewer/WorldsFragment.java
@@ -1,10 +1,73 @@
 package com.andriizastupailo.xyrality.worlds.worldsviewer;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
- * Created by andriizastupailo on 06.02.18.
+ * Handle world items
  */
 
 public class WorldsFragment extends Fragment {
+    private RecyclerView mWorldsRecyclerView;
+    private WorldAdapter mAdapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_worlds_list, container, false);
+        mWorldsRecyclerView = (RecyclerView) view.findViewById(R.id.worlds_recycler_view);
+        mWorldsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        updateUI();
+        return view;
+    }
+
+    private void updateUI(){
+        List<World> worlds = new ArrayList<>();
+        for(int i=0; i<20; i++){
+            worlds.add(new World());
+        }
+        mAdapter = new WorldAdapter(worlds);
+        mWorldsRecyclerView.setAdapter(mAdapter);
+    }
+
+    private class WorldHolder extends RecyclerView.ViewHolder{
+
+        public WorldHolder(LayoutInflater inflater, ViewGroup parent){
+            super(inflater.inflate(R.layout.list_item_world, parent, false));
+        }
+    }
+
+    private class WorldAdapter extends RecyclerView.Adapter<WorldHolder>{
+        private List<World> mWorlds;
+
+        public WorldAdapter(List<World> worlds){
+            mWorlds = worlds;
+        }
+
+        @Override
+        public WorldHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new WorldHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(WorldHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mWorlds.size();
+        }
+    }
 }
